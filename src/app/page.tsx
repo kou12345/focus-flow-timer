3dee49c1447bd5873e723ed3e914952784649489
kou12345 <hi.0412.ko.2002@gmail.com>
2025-06-15 00:40:16 +0900

    SpotifyプロバイダーをNextAuthに追加し、ホームページからSpotifyのクライアントIDとシークレットを削除

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,7 @@
 export default function Home() {
-  const spotifyApiClientId = process.env.SPOTIFY_API_CLIENT_ID;
-  const spotifyApiClientSecret = process.env.SPOTIFY_API_CLIENT_SECRET;
-
   return (
     <div>
       <h1>Spotify API</h1>
-      <p>Client ID: {spotifyApiClientId}</p>
-      <p>Client Secret: {spotifyApiClientSecret}</p>
     </div>
   );
 }
--- src/auth.ts
+++ src/auth.ts
@@ -1,8 +1,9 @@
 import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { prisma } from "@/prisma";
+import Spotify from "next-auth/providers/spotify";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
-  providers: [],
+  providers: [Spotify],
 });
